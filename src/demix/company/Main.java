b1e1ef216c57e12bb54e76198683bf4afa59056b
Demix <demon2001@inbox.ru>
2015-12-11 20:27:44 +0300

    Добавил стартовое окно

--- src/demix/company/Main.java
+++ src/demix/company/Main.java
@@ -1,8 +1,52 @@
 package demix.company;
 
-public class Main {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main extends Frame {
+    private int countClick=0;
+    private Button myButton;
+    private Label myText;
+
+    public Main(){
+        super("Первая программа");
+        setSize(800, 800);
+        myButton = new Button("Hello");
+        myText = new Label("Hello World "+countClick);
+        add(myText, BorderLayout.NORTH);
+        add(myButton, BorderLayout.SOUTH);
+
+        myButton.addActionListener(new ActionListener() {
+                                       @Override
+                                       public void actionPerformed(ActionEvent e) {
+                                           countClick++;
+                                           myText.setText("Hello World " + countClick);
+                                       }
+                                   }
+        );
+    }
 
     public static void main(String[] args) {
-	// write your code here
+
+
+
+
+        Main myProgram = new Main();
+        myProgram.setVisible(true);
+
+
+        myProgram.addWindowListener(new WindowAdapter() {
+                                        @Override
+                                        public void windowClosing(WindowEvent e) {
+                                            super.windowClosing(e);
+                                            e.getWindow().dispose();
+                                        }
+                                    }
+        );
     }
+
+
 }
